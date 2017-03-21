2312624f78a1e6b1ef9e136e2830d31120a24235
Charles Gruenwald <charles@thinktopic.com>
2017-03-20 18:08:13 -0600

    [travis] Extract the vault token somewhere relevant in travis.

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ export VAULT_ADDR=https://thinktopic.com:8200
 # vault
 wget https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_linux_amd64.zip -O vault.zip
 unzip vault.zip
-openssl aes-256-cbc -K $encrypted_6569357b0c98_key -iv $encrypted_6569357b0c98_iv -in .vault-token.enc -out ./.vault-token -d
+openssl aes-256-cbc -K $encrypted_6569357b0c98_key -iv $encrypted_6569357b0c98_iv -in .vault-token.enc -out /home/travis/.vault-token -d
 
 license_path=secret/thinktopic/datomic-license
 
