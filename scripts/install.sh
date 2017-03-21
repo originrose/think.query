83d6431febc53948651a9f4321b7385c28b0235d
Charles Gruenwald <charles@thinktopic.com>
2017-03-20 18:05:38 -0600

    [travis] Fix testing in travis.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,9 @@
 set -u
 set -e
 
+export PATH=$PATH:$PWD
+export VAULT_ADDR=https://thinktopic.com:8200
+
 # vault
 wget https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_linux_amd64.zip -O vault.zip
 unzip vault.zip
@@ -9,9 +12,7 @@ openssl aes-256-cbc -K $encrypted_6569357b0c98_key -iv $encrypted_6569357b0c98_i
 
 license_path=secret/thinktopic/datomic-license
 
-
 export DATOMIC_USERNAME=$(vault read -field=DATOMIC_USERNAME $license_path)
 export DATOMIC_PASSWORD=$(vault read -field=DATOMIC_PASSWORD $license_path)
 
 lein with-profile test deps
-
