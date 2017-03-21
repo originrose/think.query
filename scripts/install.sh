2d7eee2ce45d60168e8e2795f36d0349daad66ed
Charles Gruenwald <charles@thinktopic.com>
2017-03-20 18:04:28 -0600

    [vault] Add access to vault.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,13 @@
 set -u
 set -e
 
+# vault
+wget https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_linux_amd64.zip -O vault.zip
+unzip vault.zip
+openssl aes-256-cbc -K $encrypted_6569357b0c98_key -iv $encrypted_6569357b0c98_iv -in .vault-token.enc -out ./.vault-token -d
+
 license_path=secret/thinktopic/datomic-license
+
 
 export DATOMIC_USERNAME=$(vault read -field=DATOMIC_USERNAME $license_path)
 export DATOMIC_PASSWORD=$(vault read -field=DATOMIC_PASSWORD $license_path)
