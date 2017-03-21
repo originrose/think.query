97be8fd98ac42a99221d6a90fcbf9a14ce6cc09c
Charles Gruenwald <charles@thinktopic.com>
2017-03-21 12:20:55 -0600

    [query] Use new mechanism for managing travis tokens.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,10 +7,9 @@ export VAULT_ADDR=https://thinktopic.com:8200
 # vault
 wget https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_linux_amd64.zip -O vault.zip
 unzip vault.zip
-openssl aes-256-cbc -K $encrypted_6569357b0c98_key -iv $encrypted_6569357b0c98_iv -in .vault-token.enc -out /home/travis/.vault-token -d
+vault auth -method=github token=$GITHUB_TOKEN
 
 license_path=secret/thinktopic/datomic-license
-
 export DATOMIC_USERNAME=$(vault read -field=DATOMIC_USERNAME $license_path)
 export DATOMIC_PASSWORD=$(vault read -field=DATOMIC_PASSWORD $license_path)
 
