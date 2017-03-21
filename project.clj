0d73e4640d84cfcb3307cc49a6203d478746e046
Charles Gruenwald <charles@thinktopic.com>
2017-03-20 18:34:00 -0600

    [project] Move wagon private to only release profile.

--- project.clj
+++ project.clj
@@ -1,14 +1,21 @@
 (defproject thinktopic/think.query "0.1.1-SNAPSHOT"
   :description "A query language for clojure."
   :url "http://github.com/thinktopic/think.query"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;[org.apache.httpcomponents/httpclient "4.5.3"]
-                 ;[s3-wagon-private "1.3.0" :exclusions [commons-logging]]
-                 ]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
-  :profiles {:test {:dependencies [[com.datomic/datomic-pro "0.9.5530"]]}}
+  :profiles {:release {:plugins [[s3-wagon-private "1.3.0"]]
+                       :repositories {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
+                                                    :no-auth true
+                                                    :releases false}
+                                      "releases"  {:url "s3p://thinktopic.jars/releases/"
+                                                   :no-auth true
+                                                   :snapshots false
+                                                   :sign-releases false}}}
+             :test {:dependencies [[com.datomic/datomic-pro "0.9.5530"]]
+                    :repositories  {"my.datomic.com" {:url      "https://my.datomic.com/repo"
+                                                      :username :env/DATOMIC_USERNAME
+                                                      :password :env/DATOMIC_PASSWORD}}}}
 
-  :plugins [[s3-wagon-private "1.3.0" :exclusions [commons-logging]]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -16,15 +23,4 @@
                   ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]]
-
-  :repositories  {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
-                                :no-auth true
-                                :releases false}
-                  "releases"  {:url "s3p://thinktopic.jars/releases/"
-                               :no-auth true
-                               :snapshots false
-                               :sign-releases false}
-                  "my.datomic.com" {:url      "https://my.datomic.com/repo"
-                                    :username :env/DATOMIC_USERNAME
-                                    :password :env/DATOMIC_PASSWORD}})
+                  ["vcs" "push"]])
