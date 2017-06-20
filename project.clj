ddd5f23a9e5c2a55d8d04d7677ab1f6092f8f45b
Charles Gruenwald <charles@thinktopic.com>
2017-06-20 13:16:46 -0600

    [project] Update lein and move deps out of profile.

--- project.clj
+++ project.clj
@@ -3,18 +3,20 @@
   :url "http://github.com/thinktopic/think.query"
   :dependencies [[org.clojure/clojure "1.8.0"]]
 
-  :profiles {:release {:plugins [[s3-wagon-private "1.3.0"]]
-                       :repositories {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
-                                                    :no-auth true
-                                                    :releases false}
-                                      "releases"  {:url "s3p://thinktopic.jars/releases/"
-                                                   :no-auth true
-                                                   :snapshots false
-                                                   :sign-releases false}}}
-             :test {:dependencies [[com.datomic/datomic-pro "0.9.5530"]]
+  :plugins [[s3-wagon-private "1.3.0"]]
+
+  :profiles {:test {:dependencies [[com.datomic/datomic-pro "0.9.5530"]]
                     :repositories  {"my.datomic.com" {:url      "https://my.datomic.com/repo"
                                                       :username :env/DATOMIC_USERNAME
                                                       :password :env/DATOMIC_PASSWORD}}}}
+
+  :repositories {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
+                               :no-auth true
+                               :releases false}
+                 "releases"  {:url "s3p://thinktopic.jars/releases/"
+                              :no-auth true
+                              :snapshots false
+                              :sign-releases false}}
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
@@ -24,5 +26,4 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
-
-  :aliases {"release" ["with-profile" "release" "release" ]})
+  )
