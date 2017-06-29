3be0d0582887c99fe95b188d5d251f8d9d4910b2
Charles Gruenwald <charles@thinktopic.com>
2017-06-28 18:05:16 -0600

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.query "0.1.2"
+(defproject thinktopic/think.query "0.1.3-SNAPSHOT"
   :description "A query language for clojure."
   :url "http://github.com/thinktopic/think.query"
   :dependencies [[org.clojure/clojure "1.8.0"]]
