83a622585688438f93f91e2d8060235f34768c5b
Jon Rose <jonrose08@gmail.com>
2017-07-12 12:08:21 -0600

    adding missing js dep

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject thinktopic/think.query "0.1.4-SNAPSHOT"
+(defproject thinktopic/think.query "0.1.5"
   :description "A query language for clojure."
   :url "http://github.com/thinktopic/think.query"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 ]
 
-  :plugins [[s3-wagon-private "1.3.0"]]
+  :plugins [[s3-wagon-private "1.3.0"]
+            [cljsjs/chance "0.7.3-0"]]
 
   :profiles {:test {:dependencies [[com.datomic/datomic-pro "0.9.5530"]]
                     :repositories  {"my.datomic.com" {:url      "https://my.datomic.com/repo"
