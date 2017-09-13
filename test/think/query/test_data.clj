8ff0a44666973f8b6964a0d0ecd801ee00ba9ed8
Charles Gruenwald <charles@thinktopic.com>
2017-09-13 12:16:03 -0600

    [query/filter] Add support for filtering by date.

--- test/think/query/test_data.clj
+++ test/think/query/test_data.clj
@@ -2,6 +2,7 @@
 
 (def alice
   {:user/email "alice@foo.com"
+   :user/created #inst "2017-09-12T18:24:09.556-00:00"
    :user/age 23
    :user/first-name "Alice"
    :user/last-name "Foo"})
@@ -9,6 +10,7 @@
 (def bob
   {:user/email "bob@foo.com"
    :user/age 27
+   :user/created #inst "2017-09-12T18:24:10.556-00:00"
    :user/first-name "Bob"
    :user/friends ["Alice"]
    :user/last-name "Foo"})
